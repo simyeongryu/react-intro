4156860662371e2229d9943d7afcb0393ffbbd78
simyeongryu <rsm0503@gmail.com>
2020-04-01 13:17:30 +0900

    css 수정

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./MovieDetail.css";
 class MovieDetail extends React.Component {
   componentDidMount = () => {
     const { location, history } = this.props;
@@ -17,7 +17,30 @@ class MovieDetail extends React.Component {
     // location.state 값이 있으면 정상값 return
     // 없으면 null 리턴 후 componentDidMount에 의해 home 으로 redirect.
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      return (
+        <div className="movie-detail">
+          <img
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          />
+          <div className="movie__data-detail">
+            <h3 className="movie__title-detail">{location.state.title}</h3>
+            <h5 className="movie__year-detail">{location.state.year}</h5>
+            <h5 className="movie__rating-detail">
+              평점: {location.state.rating}/10.0
+            </h5>
+            <ul className="movie__genres-detail">
+              {location.state.genres.map((genre, index) => (
+                <li key={index} className="genres__genre-detail">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary-detail">{location.state.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
